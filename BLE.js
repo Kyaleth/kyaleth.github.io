d570de4ec0bf56a98d6b4e1bf7797ea149a9f2a1
Pawelek <55713376+Kyaleth@users.noreply.github.com>
2022-07-29 17:19:45 +0200

    Update BLE.js

--- BLE.js
+++ BLE.js
@@ -1,3 +1,22 @@
+let connectButton = document.getElementById('connect');
+let disconnectButton = document.getElementById('disconnect');
+
+
+// Connect to the device on Connect button click
+connectButton.addEventListener('click', function() {
+  isWebBLEAvailable();
+});
+  
+if (isWebBLEAvailable()) {
+  getDeviceInfo()
+}
+
+
+// Disconnect from the device on Disconnect button click
+// disconnectButton.addEventListener('click', function() {
+//   disconnect();
+// });
+
 function isWebBLEAvailable() {
   if (!navigator.bluetooth) {
      console.log('Web Bluetooth is not available!')
@@ -5,3 +24,20 @@ function isWebBLEAvailable() {
   }
   return true
 }
+
+function getDeviceInfo() {
+  let options = {
+    acceptAllDevice: true
+  }
+  
+  console.log('Requesting BLE device info...')
+  navigator.bluetooth.requestDevice(options).then(device => {
+    console.log('Name: ' + device.name)
+  }).catch(error => {
+    console.log('Request device error: ' + error)
+  })
+}
+
+
+                                                  
+                                                  }
